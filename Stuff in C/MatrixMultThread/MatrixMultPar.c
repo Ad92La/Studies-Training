1c06c57f7468844bdc613746b8cdc3aaf28e05d9
Ad92La <a.ladner@gmx.net>
2024-02-26 11:48:59 +0100

    Matrixmultiplications added

--- Stuff in C/MatrixMultThread/MatrixMultPar.c
+++ Stuff in C/MatrixMultThread/MatrixMultPar.c
@@ -1,230 +1,170 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <ctime>
+#include <iostream>
+// Die Dimensionen sind zwar fix am Übungszettel vorgegeben,
+// aber prinzipiell sollte man sie trotzdem im Programm nicht hart-coden.
+// Wenn man sie als Konstanten definiert, kann man sie später leichter ändern.
+#define A_DIMX	10
+#define B_DIMY	12
+#define S_DIM	20
 
+#define MEASUREMENTS        5
+#define HOW_MUCH_THREADS    10
+// Makro, welches einen 2-dimensionalen Index in einem 1-dimensionalen Index umwandelt
+// x .. x-Index (Anzahl der Zeilen)
+// y .. y-Index (Anzahl der Spalten)
+// s .. Größe der y-Dimension (Max. Anzahl der Spalten)
+#define MATINDEX(x,y,s)		((x)*(s)+(y))
 
-/**
- * The problem size, which is proportional to the number of executed instructions, is given by following formula:
- * problemsize = MATRIX_SIZE * MATRIX_SIZE * ITERATION_COUNT
- *
- * Using the same problemsize one can change the granularity of parallelism by using different values for ITERATION_COUNT
- * (and adjusting MATRIX_SIZE accordingly). The higher ITERATION_COUNT the more fine-granular the parallelism.
- *
- * The granularity of parallelism is represented by the measured "barrier activations per second".
- */
+// Die Matrizen A, B und C
+// Diese als globale Variablen zu definieren, macht es für die Threads einfacher, darauf zuzugreifen
+int *A, *B, *C;
 
+// Struktur für die Datenübergabe an die Threads
+// x .. Zeile der A Matrix
+// y .. Spalte der B Matrix
+typedef struct {
+    int x_start;
+    int x_count;
+    int y;
+} thread_data_t;
 
-// How many threads should be used
-#define  THREAD_COUNT     2
+// Funktion, welche von den Threads ausgeführt wird
+void* thread_func(void* arg) {
 
+    //Threaddaten
+    thread_data_t * data = (thread_data_t*)arg;
 
-// The number of measurements used to calculate the average execution time
-#define  MEASUREMENTS     1
-
-
-/**
- * Here are some example values given for ITERATION_COUNT and MATRIX_SIZE which have roughly the same problem size
- * but different granularity of parallelism.
- *
- * Attention: A matrix size of 25000 needs 25000*25000*8 Bytes (~5GB) of Memory. Make sure to not exceed the amount of memory in your system!
- */
-
-//#define  ITERATION_COUNT  20
-//#define  MATRIX_SIZE      25000
-
-#define  ITERATION_COUNT  500
-#define  MATRIX_SIZE      5000
-
-//#define  ITERATION_COUNT  2000
-//#define  MATRIX_SIZE      2500
-
-//#define  ITERATION_COUNT  8000
-//#define  MATRIX_SIZE      1250
-
-//#define  ITERATION_COUNT  50000
-//#define  MATRIX_SIZE      500
-
-//#define  ITERATION_COUNT  800000
-//#define  MATRIX_SIZE      125
-
-
-
-
-
-
-typedef struct  {
-    int thread_id;
-    double** matrix;
-    int matrix_size;
-    double* right_hand_side;
-    double* solution;
-    int range_start;
-    int range_size;
-} thread_info_t;
-
-
-pthread_barrier_t barrier;
-
-
-
-void solve_jacobi_serial(double** matrix, int matrix_size, double* right_hand_side, double* solution) {
-    double* tempsolution = (double*)malloc(MATRIX_SIZE*sizeof(double));
-
-    // Iteration loop
-    for (int i = 0; i < ITERATION_COUNT; i++){
-        // loop through rows
-        for (int j = 0; j < matrix_size; j++){
-            double sigma_value = 0;
-            // loop through columns
-            for (int k = 0; k < matrix_size; k++){
-                if (j != k) {
-                    sigma_value += matrix[j][k] * solution[k];
-                }
+    //Matrixberechnung
+    for (int x = data->x_start; x < data->x_start+data->x_count; x++) {
+        for (int y = 0; y < B_DIMY; y++) {
+            int result = 0;
+            for (int i = 0; i < S_DIM; i++) {
+                result += A[MATINDEX(x,i,S_DIM)]* B[MATINDEX(i,y,B_DIMY)];
             }
-            tempsolution[j] = (right_hand_side[j] - sigma_value) / matrix[j][j];
+            C[MATINDEX(x,y,B_DIMY)] = result;
         }
-        memcpy(solution, tempsolution, MATRIX_SIZE);
-
-        //printf("\rIteration #%6d/%d", i+1, ITERATION_COUNT);
     }
-    //printf("\n");
-}
-
-
-void* solve_jacobi_parallel(void* targ) {
-    thread_info_t* info = (thread_info_t*)targ;
-    double* tempsolution = (double*)malloc(MATRIX_SIZE*sizeof(double));
-
-    //printf("Thread #%d - range %d, %d\n", info->thread_id, info->range_start, info->range_size);
-
-    // Iteration loop
-    for (int i = 0; i < ITERATION_COUNT; i++){
-        // loop through rows
-        for (int j = info->range_start; j < info->range_start+info->range_size; j++){
-            double sigma_value = 0;
-            // loop through columns
-            for (int k = 0; k < info->matrix_size; k++){
-                if (j != k) {
-                    sigma_value += info->matrix[j][k] * info->solution[k];
-                }
-            }
-            tempsolution[j] = (info->right_hand_side[j] - sigma_value) / info->matrix[j][j];
-        }
-
-        // Here we need to synchronise before we continue with the next iteration
-        pthread_barrier_wait(&barrier);
-        memcpy(info->solution+info->range_start, tempsolution+info->range_start, info->range_size);
-
-        //if (info->thread_id == 0) {
-        //	printf("\rIteration #%6d/%d", i+1, ITERATION_COUNT);
-        //}
-    }
-    //if (info->thread_id == 0) {
-    //	printf("\n");
-    //}
-    free(tempsolution);
     return NULL;
 }
 
+// Gebe die übergebene Matrix aus
+// mat .. Matrix,
+// x .. Anzahl der Zeilen
+// y .. Anzahl der Spalten
+//void printMatrix(int* mat, int x, int y) {
+//    int min = INT_MAX, max = INT_MIN, sum = 0;
+//    // Schleife über alle Zeilen
+//    for (int i = 0; i < x; i++) {
+//        int firstElement = 1;
+//        // Schleife über alle Spalten
+//        for (int j = 0; j < y; j++) {
+//            // Für das erste Element müssen wir davor kein '\t' ausgeben
+//            if (firstElement) {
+//                firstElement = 0;
+//            } else {
+//                printf("\t");
+//            }
+//            int e = mat[MATINDEX(i,j,y)];
+//            // Gebe Element aus
+//            printf("%d", e);
+//            // Berechne min
+//            if (e < min) {
+//                min = e;
+//            }
+//            // Berechne max
+//            if (e > max) {
+//                max = e;
+//            }
+//            // Berechne Summe
+//            sum += e;
+//        }
+//        printf("\n");
+//    }
+//    // Gebe min, max und summe aus
+//    printf("Minimaler Wert: %d; Maximaler Wert: %d; Summe aller Werte: %d\n", min, max, sum);
+//}
 
-double gettime() {
-    struct timespec time;
+double getTime() {
+    struct timespec time{};
     clock_gettime(CLOCK_REALTIME, &time);
     return (double)time.tv_sec + (double)time.tv_nsec/1000000000.0;
 }
 
 
+int main() {
 
-int main(int argc, char **argv) {
+    srand(time(NULL));
+    // Alloziere Speicher für die Matrizen
+    A = static_cast<int *>(malloc(sizeof(int) * A_DIMX * S_DIM));
+    B = static_cast<int *>(malloc(sizeof(int) * S_DIM * B_DIMY));
+    C = static_cast<int *>(malloc(sizeof(int) * A_DIMX * B_DIMY));
 
-    // Declare variables
-    int matrix_size = MATRIX_SIZE;
-    double **matrix = (double**)malloc(matrix_size * sizeof(double*));
-    for (int i = 0; i < matrix_size; i++) {
-        matrix[i] = (double*)malloc(matrix_size*sizeof(double));
+    // Fülle die Matrizen A und B mit Zufallswerten (zwischen 0 und 10)
+    for (int i = 0; i < 10*20; i++) {
+        A[i] = rand() % 10;
     }
-    double* right_hand_side = (double*)malloc(matrix_size*sizeof(double));
-    double* solution = (double*)malloc(matrix_size*sizeof(double));
+    for (int i = 0; i < 20*12; i++) {
+        B[i] = rand() % 10;
+    }
 
-    printf("matrix size: %d\niteration count: %d\nthread count: %d\n", matrix_size, ITERATION_COUNT, THREAD_COUNT);
+    double durchschnittRuntime = 0.0;
 
-
-    // Initialise barrier if more than one thread
-#if THREAD_COUNT > 1
-    pthread_barrier_init(&barrier, NULL, THREAD_COUNT);
-#endif
-
-
-    // measurement loop
-    double avgtime = 0.0;
     for (int m = 0; m < MEASUREMENTS; m++) {
 
-        // Initialize matrix and vectors
-        for (int i = 0; i < matrix_size; i++) {
-            // Jacobi only works correctly with strictly diagonally dominant matrices
-            // Therefore we need to ensure that our random matrix is strictly diagonally dominant
-            double row_sum = 0;
-            for (int j = 0; j < matrix_size; j++) {
-                matrix[i][j] = 1 + rand() % 20;
-                if (j != i) {
-                    row_sum += matrix[i][j];
-                }
-            }
-            matrix[i][i] = row_sum + 1;
-        }
-        for (int i = 0; i < matrix_size; i++) {
-            right_hand_side[i] = rand() % 20;
-            solution[i] = right_hand_side[i];
-        }
+#if HOW_MUCH_THREADS > 1
+        //Startzeit
+        double startTime = getTime();
+        thread_data_t threadData[HOW_MUCH_THREADS];
+            pthread_t threads[HOW_MUCH_THREADS];
 
-        // if only one thread then execute serial version
-#if THREAD_COUNT == 1
+                for (int i = 0; i < HOW_MUCH_THREADS; ++i) {
+                    //Füllen der Datenstruktur
+                    threadData[i].x_start = (A_DIMX / HOW_MUCH_THREADS
+                        ) *i;
+                        if (i < HOW_MUCH_THREADS -1) {
+                                threadData[i].x_count = A_DIMX / HOW_MUCH_THREADS ;
+                                } else {
+                                    threadData[i].x_count = threadData[i].x_count - threadData[i].x_start;
+                                }
+                                pthread_create(&threads[i], NULL, thread_func, &threadData[i]);
+                            }
 
-        double starttime = gettime(); // starttime
+                            for (int i = 0; i < HOW_MUCH_THREADS; ++i) {
+                                pthread_join(threads[i], NULL);
+                            }
 
-			solve_jacobi_serial(matrix, matrix_size, right_hand_side, solution);
-
-			double endtime = gettime(); // endtime
-			avgtime += endtime-starttime;
-			printf("Time: %lf s\n", endtime-starttime);
-
-		// if more than one thread create threads and execute parallel version
+                            //Messung endzeit
+                            double endTime = getTime();
+                            double duration = endTime - startTime;
+                            std::cout << duration << " s" << std::endl;
+                            durchschnittRuntime += duration;
 #else
-        thread_info_t infos[THREAD_COUNT];
-        pthread_t threads[THREAD_COUNT];
-
-        double starttime = gettime(); // starttime
-
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            infos[i].thread_id = i;
-            infos[i].matrix = matrix;
-            infos[i].matrix_size = matrix_size;
-            infos[i].right_hand_side = right_hand_side;
-            infos[i].solution = solution;
-            infos[i].range_start = (MATRIX_SIZE / THREAD_COUNT) * i;
-            if (i == THREAD_COUNT-1) {
-                infos[i].range_size = MATRIX_SIZE - infos[i].range_start;
-            } else {
-                infos[i].range_size = (MATRIX_SIZE / THREAD_COUNT);
+//Startzeit
+        double startTime = getTime();
+        for (int x = 0; x < A_DIMX; x++) {
+            for (int y = 0; y < B_DIMY; y++) {
+                int result = 0;
+                for (int i = 0; i < S_DIM; i++) {
+                    result += A[MATINDEX(x,i,S_DIM)] * B[MATINDEX(i,y,B_DIMY)];
+                }
+                C[MATINDEX(x,y,B_DIMY)] = result;
             }
-            pthread_create(&threads[i], NULL, solve_jacobi_parallel, &infos[i]);
-        }
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            pthread_join(threads[i], NULL);
         }
 
-        double endtime = gettime(); // endtime
-        avgtime += endtime-starttime;
-        printf("Time: %lf s\n", endtime-starttime);
-        printf("Barrier activations per second: %lf\n", (double)ITERATION_COUNT/(endtime-starttime));
+        double endTime = getTime();
+        double duration = endTime - startTime;
+        std::cout << duration << " s, laufzeit" << std::endl;
+        durchschnittRuntime += duration;
+
 #endif
     }
+    std::cout << "Durchschnittliche ZEIT: " <<durchschnittRuntime / MEASUREMENTS <<  std::endl;
+    // Gebe Ergebnis aus
+    //    printMatrix(C, A_DIMX, B_DIMY);
 
-    printf("Average time: %lf s\n", avgtime / (double)MEASUREMENTS);
-
-#if THREAD_COUNT > 1
-    pthread_barrier_destroy(&barrier);
-#endif
+    return 0;
 }
